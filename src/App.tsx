6db7ad95cc3c101e3f714b5eda1606a4338dea09
mahmoudjd <mahmoudjd452@gmail.com>
2025-05-01 23:18:25 +0200

    Enhance App with Hero-component

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,26 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Hero from "./components/Hero";
 
 function App() {
-  return (
-    <div className="flex flex-col min-h-screen ">
-      <Router>
-        <Header />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/skills" element={<Skills />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-      </Router>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="flex flex-col min-h-screen bg-gradient-to-b from-gray-900 to-black text-white">
+            <Router>
+                <Header/>
+                <main className="flex-grow">
+                    <Hero/>
+                    <Routes>
+                        <Route path="/" element={<About/>}/>
+                        <Route path="/projects" element={<Projects/>}/>
+                        <Route path="/skills" element={<Skills/>}/>
+                        <Route path="/contact" element={<Contact/>}/>
+                    </Routes>
+                </main>
+                <Footer/>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
