8b9c2419e783939798541e297aa73db5da2baf96
mahmoudjd <mahmoudjd452@gmail.com>
2025-05-01 23:31:12 +0200

    Update the skills

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,40 +1,46 @@
 export const skills = [
-  {
-    category: "Text Processing Programs",
-    details: ["Word", "PowerPoint", "LaTeX"],
-  },
-  {
-    category: "Programming Languages",
-    details: ["Python", "Java", "JavaScript", "TypeScript", "C#"],
-  },
-  {
-    category: "Technologies and Frameworks",
-    details: [
-      "React",
-      "React Native",
-      "Express.js",
-      "Node.js",
-      "MongoDB",
-      "Apache Spark",
-      "Ray.io",
-      "Cheerio.js",
-      "RESTful-API",
-      "Git",
-      "Tailwindcss",
-    ],
-  },
-  { category: "Operating Systems", details: ["MacOS", "Linux"] },
-  {
-    category: "Additional Skills",
-    details: ["Driver's license Class B (06.2022)"],
-  },
-  { category: "Hobbies", details: ["Playing football", "Cycling"] },
-  {
-    category: "Languages",
-    details: [
-      "Arabic (Native)",
-      "German (Fluent - C1)",
-      "English (Basic knowledge)",
-    ],
-  },
+    {
+        category: "Text Processing Programs",
+        details: ["Word", "PowerPoint", "LaTeX"],
+    },
+    {
+        category: "Programming Languages",
+        details: ["Python", "Java", "JavaScript", "TypeScript", "C#"],
+    },
+    {
+        category: "Technologies and Frameworks",
+        details: [
+            "React",
+            "React Native",
+            "Next.js",
+            "Flask",
+            "Express.js",
+            "Node.js",
+            "MongoDB",
+            "Apache Spark",
+            "Ray.io",
+            "Cheerio.js",
+            "RESTful-API",
+            "Git",
+            "GitHub",
+            "YouTrack",
+            "Tailwindcss",
+            "SQLite",
+            "Agile methodologies"
+        ],
+    },
+    {category: "Operating Systems", details: ["MacOS", "Linux"]},
+    {
+        category: "Additional Skills",
+        details: ["Driver's license Class B (06.2022)"],
+    },
+    {category: "Hobbies", details: ["Playing football", "Cycling", "Programming"]},
+    {
+        category: "Languages",
+        details: [
+            "Arabic (Native)",
+            "German (Fluent - C1)",
+            "English (Basic knowledge - B1)",
+        ],
+    },
 ];
