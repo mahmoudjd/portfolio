d7139e2ddbdb773efdadb9ff2eff3b0143d467d6
mahmoudjd <mahmoudjd452@gmail.com>
2025-05-04 11:18:55 +0200

    Improve style of list component

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -3,6 +3,7 @@ import React from "react";
 interface ListProps {
     children: React.ReactNode;
     grid?: boolean;
+    wrap?: boolean;
     className?: string;
     cols?: string;
 }
@@ -10,12 +11,15 @@ interface ListProps {
 export function List({
                          children,
                          grid = false,
+                         wrap = false,
                          className = "",
                          cols = "sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3",
                      }: ListProps) {
     const baseClass = grid
         ? `grid gap-6 ${cols}`
-        : "flex flex-col space-y-6";
+        : wrap
+            ? "flex flex-wrap gap-2 py-4"
+            : "flex flex-col space-y-6";
 
     return (
         <div className={`${baseClass} ${className}`}>
