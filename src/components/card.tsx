5bb981fe42771afc2bb8c48566659ba6eec63731
mahmoudjd <mahmoudjd452@gmail.com>
2025-05-04 11:18:43 +0200

    Improve style of card component

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,7 +1,11 @@
-export function Card({children}: { children: React.ReactNode }) {
+interface CardProps {
+    children: React.ReactNode
+    className?: string
+}
+export function Card({children, className}: CardProps) {
     return (
         <div
-            className="bg-gray-800 p-4 rounded-2xl overflow-hidden shadow-2xl hover:shadow-green-500/40 transition-transform duration-300 hover:-translate-y-1">
+            className={"bg-gray-800 p-4 rounded-2xl overflow-hidden shadow-2xl hover:shadow-green-500/40 transition-transform duration-300 hover:-translate-y-1 "+ className}>
             {children}
         </div>)
 }
